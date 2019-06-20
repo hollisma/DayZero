8ccdbdd2f38df9aa57241ac2426b18ce78dfc60b
Hollis Ma <hollism@princeton.edu>
2019-06-20 18:32:49 -0400

    Feedback model

---
+++ models/Feedback.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const FeedbackSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    require: true
+  },
+  Group: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "group"
+  },
+  time: {
+    type: Date,
+    default: Date.now
+  },
+  q1: {
+    type: String
+  },
+  q2: {
+    type: String
+  },
+  q3: {
+    type: String
+  }
+});
+
+module.exports = Feedback = mongoose.model("feedback", FeedbackSchema);
