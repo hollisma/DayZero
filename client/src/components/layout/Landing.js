eabe8509a6661307e10cc91d9421259d64c1fd29
Hollis Ma <hollism@princeton.edu>
2019-06-30 20:28:44 -0400

    second section css and content but justify:left not working

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -19,32 +19,23 @@ const Landing = () => {
       {/* Second section */}
       <div id="second" className="blu chunk">
         <div className="ui vertical stripe segment">
+          <h1>How Day Zero Works</h1>
           <div className="ui middle aligned stackable grid container">
-            <div>
-              <h1>How Day Zero Works</h1>
-              <div className="section">
-                <img src={require("../../img/three-group")} />
-                <p>
-                  Tell us what kind of people you'd like to meet. Entrepreneurs?
-                  Aspiring artists and musicians? People who love building
-                  rockets?
-                </p>
-              </div>
-              <div className="section">
-                <img src={require("../../img/three-group")} />
-                <p>
-                  Set your calendar for the times you're free to grab a meal of
-                  coffee
-                </p>
-              </div>
-              <div className="section">
-                <img src={require("../../img/three-group")} />
-                <p>
-                  Based on your interests, Day Zero will automatically match you
-                  with three other people to have a meal or coffee!
-                </p>
-              </div>
-            </div>
+            <img src={require("../../img/three-group")} />
+            <p>
+              Tell us what kind of people you'd like to meet. Entrepreneurs?
+              Aspiring artists and musicians? People who love building rockets?
+            </p>
+            <img src={require("../../img/three-group")} />
+            <p>
+              Set your calendar for the times you're free to grab a meal of
+              coffee
+            </p>
+            <img src={require("../../img/three-group")} />
+            <p>
+              Based on your interests, Day Zero will automatically match you
+              with three other people to have a meal or coffee!
+            </p>
           </div>
         </div>
       </div>
