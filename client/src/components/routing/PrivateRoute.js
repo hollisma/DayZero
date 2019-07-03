7e979269083e60a2a72a9ae456ba67b524f3529e
Hollis Ma <hollism@princeton.edu>
2019-07-02 21:29:05 -0400

    added default routes for the different user types and implemented them into privateRoute

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -2,6 +2,13 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import {
+  GUEST_DEFAULT,
+  REGISTERED_DEFAULT,
+  PROFILED_DEFAULT,
+  MET_DEFAULT
+} from "../routing/default_types";
+import { GUEST, REGISTERED, PROFILED, MET } from "../../actions/types";
 
 const PrivateRoute = ({
   component: Component,
@@ -9,12 +16,28 @@ const PrivateRoute = ({
   access,
   ...rest
 }) => {
-  if (access == null) access = [];
+  access = access == null ? [] : access;
+
+  var defaultRoute = GUEST_DEFAULT;
+  const userType = user ? user.user_type : GUEST;
+  switch (userType) {
+    case REGISTERED:
+      defaultRoute = REGISTERED_DEFAULT;
+      break;
+    case PROFILED:
+      defaultRoute = PROFILED_DEFAULT;
+      break;
+    case MET:
+      defaultRoute = MET_DEFAULT;
+      break;
+    default:
+      defaultRoute = GUEST_DEFAULT;
+      break;
+  }
+  console.log("defaultRoute", defaultRoute);
 
   const userTypeIncluded =
-    user &&
-    (access.some(currentType => user.user_type === currentType) ||
-      access.length == 0);
+    access.some(currentType => userType === currentType) || access.length === 0;
 
   return (
     <Route
@@ -23,7 +46,7 @@ const PrivateRoute = ({
         (isAuthenticated && userTypeIncluded) || loading ? (
           <Component {...props} />
         ) : (
-          <Redirect to="login" />
+          <Redirect to={defaultRoute} />
         )
       }
     />
---
+++ client/src/components/routing/default_types.js
@@ -0,0 +1,4 @@
+export const GUEST_DEFAULT = "login";
+export const REGISTERED_DEFAULT = "profile";
+export const PROFILED_DEFAULT = "dashboard";
+export const MET_DEFAULT = "dashboard";
