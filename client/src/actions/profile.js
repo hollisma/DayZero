90ab38bea710af79c4dd28789c058b80f73dd260
Hollis Ma <hollism@princeton.edu>
2019-07-06 11:33:30 -0400

    createProfile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+const MySwal = withReactContent(Swal);
+
 import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from "./types";
 
 export const getCurrentProfile = () => async dispatch => {
@@ -14,6 +18,28 @@ export const getCurrentProfile = () => async dispatch => {
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, state: err.response.status }
+    });
+  }
+};
+
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error =>
+        MySwal.fire({ title: "Passwords do not match", type: "error" })
+      );
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
     });
   }
 };
