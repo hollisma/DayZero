4629783b06cf660043891aec6aab8b166ef77a8a
Hollis Ma <hollism@princeton.edu>
2019-07-06 10:41:55 -0400

    profile action/reducer and getProfile action

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -5,6 +5,9 @@ export const LOGIN_FAIL = "LOGIN_FAIL";
 export const USER_LOADED = "USER_LOADED";
 export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGOUT = "LOGOUT";
+export const GET_PROFILE = "GET_PROFILE";
+export const PROFILE_ERROR = "PROFILE_ERROR";
+export const UPDATE_PROFILE = "UPDATE_PROFILE";
 
 export const GUEST = "GUEST";
 export const REGISTERED = "REGISTERED";
