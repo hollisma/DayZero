c21ca33431551a3dcc1909aa747f28d9008d8f11
Hollis Ma <hollism@princeton.edu>
2019-07-09 18:05:57 -0400

    EditProfile component

--- client/src/components/profile/EditProfile.js
+++ client/src/components/profile/EditProfile.js
@@ -1,12 +1,136 @@
-import React from "react";
+import React, { useState, Fragment, useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProfile, getCurrentProfile } from "../../actions/profile";
+import { getDefaultRoute } from "../routing/default_types";
 
-const EditProfile = props => {
-  return <div>Edit Profile</div>;
+const EditProfile = ({
+  profile: { profile, loading },
+  createProfile,
+  getCurrentProfile,
+  history
+}) => {
+  const [formData, setFormData] = useState({
+    college: "",
+    major: "",
+    minor: "",
+    categories: "",
+    bio: "",
+    values: ""
+  });
+
+  useEffect(() => {
+    getCurrentProfile();
+
+    setFormData({
+      college: loading || !profile.college ? "" : profile.college,
+      major: loading || !profile.major ? "" : profile.major.join(", "),
+      minor: loading || !profile.minor ? "" : profile.minor.join(", "),
+      categories:
+        loading || !profile.categories ? "" : profile.categories.join(", "),
+      bio: loading || !profile.bio ? "" : profile.bio,
+      values: loading || !profile.values ? "" : profile.values
+    });
+  }, [loading, getCurrentProfile]);
+
+  const { college, major, minor, categories, bio, values } = formData;
+
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history, true);
+  };
+
+  return (
+    <Fragment>
+      <h1 className="larger text-primary">Create Your Profile</h1>
+      <p className="lead my-2">
+        <i className="fas fa-user" />
+        Tell us about yourself!
+      </p>
+      <small>* = required field</small>
+      <form className="ui form" onSubmit={e => onSubmit(e)}>
+        <div className="field">
+          <input
+            type="text"
+            placeholder="College"
+            name="college"
+            value={college}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="field">
+          <input
+            type="text"
+            placeholder="Major"
+            name="major"
+            value={major}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="field">
+          <input
+            type="text"
+            placeholder="Minor"
+            name="minor"
+            value={minor}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className="field">
+          <input
+            type="text"
+            placeholder="Categories"
+            name="categories"
+            value={categories}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className="field">
+          <input
+            type="text"
+            placeholder="Bio"
+            name="bio"
+            value={bio}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className="field">
+          <input
+            type="text"
+            placeholder="Values"
+            name="values"
+            value={values}
+            onChange={e => onChange(e)}
+          />
+        </div>
+
+        <input type="submit" className="ui button my-1" />
+        <button className="ui button my-1" onClick={history.goBack()}>
+          Go Back
+        </button>
+      </form>
+    </Fragment>
+  );
 };
 
 EditProfile.propTypes = {
-  /** @TODO Reference EditProfile.js in devconnector */
+  createProfile: PropTypes.func.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
 };
 
-export default EditProfile;
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { createProfile, getCurrentProfile }
+)(withRouter(EditProfile));
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -19,6 +19,5 @@ export default function(state = initialState, action) {
       };
     default:
       return state;
-      break;
   }
 }
