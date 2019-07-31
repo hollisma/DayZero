7a672370912391be55803d8c512f425671569908
hollisma <hollism@princeton.edu>
2019-07-31 17:47:22 -0400

    create schedule and scheduled user type

--- routes/api/schedule.js
+++ routes/api/schedule.js
@@ -5,7 +5,7 @@ const auth = require("../../middleware/auth");
 // Models
 const Schedule = require("../../models/Schedule");
 const User = require("../../models/User");
-const { PROFILED } = require("../../models/types");
+const { SCHEDULED } = require("../../models/types");
 
 /**
  * @route   GET api/schedule/me
@@ -20,12 +20,75 @@ router.get("/me", auth, async (req, res) => {
     );
 
     if (!schedule) {
-      return res.status(400).json({ msg: "There is no schedule for this user" });
+      return res
+        .status(400)
+        .json({ msg: "There is no schedule for this user" });
     }
 
     res.json(schedule);
   } catch (err) {
     console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+/**
+ * @route   POST api/schedule
+ * @desc    Create or update user schedule
+ * @access  Private
+ */
+router.post("/", auth, async (req, res) => {
+  const { times } = req.body;
+
+  try {
+    let schedule = await Schedule.findOne({ user: req.user.id });
+
+    if (schedule) {
+      // Update
+      schedule = await Schedule.findOneAndUpdate(
+        { user: req.user.id },
+        { times: times },
+        { new: true }
+      );
+
+      return res.json(schedule);
+    }
+
+    // Create
+    schedule = new Schedule({ times: times });
+    await schedule.save();
+
+    // Set user type to SCHEDULED
+    await User.findByIdAndUpdate(req.user.id, {
+      $set: { user_type: SCHEDULED }
+    });
+
+    res.json(schedule);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+/**
+ * @route   GET api/schedule/user/:user_id
+ * @desc    Get schedule by user ID
+ * @access  Public
+ */
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const schedule = await Schedule.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+
+    if (!schedule) return res.status(400).json({ msg: "Profile not found" });
+
+    res.json(schedule);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Schedule not found" });
+    }
     res.status(500).send("Server Error");
   }
 });
