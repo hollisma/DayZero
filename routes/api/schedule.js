bf53fa086242c760c1c4ae022e4f21965d52e827
hollisma <hollism@princeton.edu>
2019-07-31 17:35:15 -0400

    get current schedule

--- routes/api/schedule.js
+++ routes/api/schedule.js
@@ -7,4 +7,27 @@ const Schedule = require("../../models/Schedule");
 const User = require("../../models/User");
 const { PROFILED } = require("../../models/types");
 
+/**
+ * @route   GET api/schedule/me
+ * @desc    Get current user's schedule
+ * @access  Private
+ */
+router.get("/me", auth, async (req, res) => {
+  try {
+    const schedule = await Schedule.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!schedule) {
+      return res.status(400).json({ msg: "There is no schedule for this user" });
+    }
+
+    res.json(schedule);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
