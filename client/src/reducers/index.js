edb4beb64b0d0a52444ed1207ab6643c267083a6
hollisma <hollism@princeton.edu>
2019-08-01 14:16:25 -0400

    created schedule reducer and action files

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
 import profile from "./profile";
+import schedule from "./schedule";
 
-export default combineReducers({ auth, profile });
+export default combineReducers({ auth, profile, schedule });
