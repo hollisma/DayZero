45d3525f57fe3deb23bc9cb484e67e52bc8fcdac
hollisma <hollism@princeton.edu>
2019-08-13 13:23:03 -0400

    created GroupMember component

--- client/src/components/dashboard/group/Group.js
+++ client/src/components/dashboard/group/Group.js
@@ -1,9 +1,18 @@
 import React from "react";
+import GroupMember from "./GroupMember";
 import PropTypes from "prop-types";
 
 const Group = props => {
   return (
-    <div id="group">Group
+    <div id="group" className="group">
+      <h1 className="larger text-primary">Group</h1>
+      <p>Here's your group: </p>
+      <div className="group-icons">
+        <GroupMember />
+        <GroupMember />
+        <GroupMember />
+        <GroupMember />
+      </div>
     </div>
   );
 };
---
+++ client/src/components/dashboard/group/GroupMember.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GroupMember = props => {
+  return <div>hi</div>;
+};
+
+GroupMember.propTypes = {};
+
+export default GroupMember;
