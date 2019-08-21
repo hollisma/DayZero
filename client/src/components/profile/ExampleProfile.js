83320e21e78d8ef8ad553a38593b33ffd995dea3
hollisma <hollism@princeton.edu>
2019-08-21 12:24:03 -0400

    example profile css and refacotring

--- client/src/components/profile/ExampleProfile.js
+++ client/src/components/profile/ExampleProfile.js
@@ -5,29 +5,34 @@ import "./ExampleProfile.css";
 const ExampleProfile = ({ firstName }) => {
   return (
     <div>
-      <div className="outer">
-        {firstName === "max" ? (
+      {firstName === "max" ? (
+        <div className="outer">
           <img
             src={require("../../img/max_kim.png")}
             alt="Max Kim"
             className="circular-pic"
           />
-        ) : (
+
+          <div className="name">Max Kim</div>
+          <div className="about">
+            Max is the founder of a virtual reality gaming company and wants to
+            meet passionate entrepreneurs.
+          </div>
+        </div>
+      ) : (
+        <div className="outer">
           <img
             src={require("../../img/carina_lewis.png")}
             alt="Carina Lewis"
-            className="circular-pic"
+            className="circular-pic carina"
           />
-        )}
-        <div className="name">
-          {firstName === "max" ? "Max Kim" : "Carina Lewis"}
+          <div className="name carina">Carina Lewis</div>
+          <div className="about">
+            Carina is studying Philosophy and wants to meet people who are
+            interested in foreign policy and moral philosophy!
+          </div>
         </div>
-        <div className="about">
-          {firstName === "max"
-            ? "Max is the founder of a virtual reality gaming company and wants to meet passionate entrepreneurs."
-            : "Carina is studying Philosophy and wants to meet people who are interested in foreign policy and moral philosophy!"}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
