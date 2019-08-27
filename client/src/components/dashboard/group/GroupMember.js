86d6e4c9d53073e76aa2016a60e6c3eac0e7d3a6
hollisma <hollism@princeton.edu>
2019-08-27 12:36:44 -0400

    default pic

--- client/src/components/dashboard/group/GroupMember.js
+++ client/src/components/dashboard/group/GroupMember.js
@@ -14,7 +14,40 @@ const GroupMember = ({ member_id, name, email, college, major }) => {
 
   return (
     <div className="group-member">
-      <Avatar email={email} name={name} size="100" round className="avatar" />
+      <Avatar
+        email={email}
+        name={name}
+        size="100"
+        round
+        className="avatar"
+        // color={
+        //   "#" +
+        //   (function co(lor) {
+        //     return (lor += [
+        //       0,
+        //       1,
+        //       2,
+        //       3,
+        //       4,
+        //       5,
+        //       6,
+        //       7,
+        //       8,
+        //       9,
+        //       "a",
+        //       "b",
+        //       "c",
+        //       "d",
+        //       "e",
+        //       "f"
+        //     ][Math.floor(Math.random() * 16)]) && lor.length === 6
+        //       ? lor
+        //       : co(lor);
+        //   })("")
+        // }
+        color="#ffffff"
+        src="https://i.stack.imgur.com/dr5qp.jpg"
+      />
       {res || "Searching..."}
     </div>
   );
