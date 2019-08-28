ba1e256c3fe93cda6a1cfd484d455ba6a643731d
hollisma <hollism@princeton.edu>
2019-08-27 20:11:13 -0400

    updated passport

--- routes/passport.js
+++ routes/passport.js
@@ -1,6 +1,7 @@
 const config = require("config");
 const passport = require("passport");
 const GoogleTokenStrategy = require("passport-google-token").Strategy;
+const FacebookTokenStrategy = require("passport-facebook-token");
 const { REGISTERED } = require("../models/types");
 
 const User = require("../models/User");
@@ -13,33 +14,93 @@ module.exports = function() {
         clientSecret: config.get("googleAuth").clientSecret
       },
       (accessToken, refreshToken, profile, done) =>
-        User.findOne(
+        User.findOneAndUpdate(
           {
-            "googleProvider.id": profile.id
+            email: profile.emails[0].value
           },
-          (err, user) => {
-            if (!user) {
-              let newUser = new User({
-                name: profile.displayName,
-                email: profile.emails[0].value,
-                user_type: REGISTERED,
-                googleProvider: {
-                  id: profile.id,
-                  token: accessToken
-                }
-              });
-
-              newUser.save((error, savedUser) => {
-                if (error) {
-                  console.log(error);
-                }
-                return done(error, savedUser);
-              });
-            } else {
-              return done(err, user);
+          {
+            $set: {
+              name: profile.displayName,
+              email: profile.emails[0].value,
+              googleProvider: {
+                id: profile.id,
+                token: accessToken
+              }
+            },
+            $setOnInsert: {
+              user_type: REGISTERED
             }
+          },
+          { upsert: true, new: true },
+          (err, doc) => {
+            if (err) console.log(err);
+            if (!doc.user_type) {
+            }
+            return done(err, doc);
           }
         )
+    )
+  );
+
+  passport.use(
+    new FacebookTokenStrategy(
+      {
+        clientID: config.get("facebookAuth").clientID,
+        clientSecret: config.get("facebookAuth").clientSecret
+      },
+      (accessToken, refreshToken, profile, done) =>
+        User.findOneAndUpdate(
+          {
+            email: profile.emails[0].value
+          },
+          {
+            $set: {
+              name: profile.displayName,
+              email: profile.emails[0].value,
+              facebookProvider: {
+                id: profile.id,
+                token: accessToken
+              }
+            },
+            $setOnInsert: {
+              user_type: REGISTERED
+            }
+          },
+          { upsert: true, new: true },
+          (err, doc) => {
+            if (err) console.log(err);
+            if (!doc.user_type) {
+            }
+            return done(err, doc);
+          }
+        )
+      // User.findOne(
+      //   {
+      //     email: profile.emails[0].value
+      //   },
+      //   (err, user) => {
+      //     if (!user) {
+      //       let newUser = new User({
+      //         name: profile.displayName,
+      //         email: profile.emails[0].value,
+      //         user_type: REGISTERED,
+      //         facebookProvider: {
+      //           id: profile.id,
+      //           token: accessToken
+      //         }
+      //       });
+
+      //       newUser.save((error, savedUser) => {
+      //         if (error) {
+      //           console.log(error);
+      //         }
+      //         return done(error, savedUser);
+      //       });
+      //     } else {
+      //       return done(err, user);
+      //     }
+      //   }
+      // )
     )
   );
 };
