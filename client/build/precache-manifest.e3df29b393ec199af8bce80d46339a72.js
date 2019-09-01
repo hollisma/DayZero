96ad75472d4dc27d674ea38e8cf39603327890af
Hollis Ma <hollism@princeton.edu>
2019-09-01 13:31:55 +0000

    build project and deployed on EC2

---
+++ client/build/precache-manifest.e3df29b393ec199af8bce80d46339a72.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124cd4e7c41fde707264f24a82d48f2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7a87cc764545021b999",
+    "url": "/static/css/main.7be0b479.chunk.css"
+  },
+  {
+    "revision": "6147bcb4cc9e9f473e23",
+    "url": "/static/js/2.e2f15f02.chunk.js"
+  },
+  {
+    "revision": "a7a87cc764545021b999",
+    "url": "/static/js/main.65a5317b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8b4916784994c4e8a619be20716586d7",
+    "url": "/static/media/carina_lewis.8b491678.png"
+  },
+  {
+    "revision": "2b62f27ce08bdf7caec391088ba97df2",
+    "url": "/static/media/max_kim.2b62f27c.png"
+  },
+  {
+    "revision": "4bd39f5649e06639ef5243b1dd70bc50",
+    "url": "/static/media/princeton.4bd39f56.bin"
+  },
+  {
+    "revision": "f3814fe2b8a4d2c768fbea5d83e87ea9",
+    "url": "/static/media/stanford.f3814fe2.bin"
+  },
+  {
+    "revision": "c99435c7af2b1fa41cc469f2a669f717",
+    "url": "/static/media/three-group.c99435c7.bin"
+  }
+]);
