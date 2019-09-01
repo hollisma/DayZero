8899c214774e3be1153e03e211db218617184c94
hollisma <hollism@princeton.edu>
2019-09-01 14:55:40 -0400

    split out landing page css

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -3,13 +3,13 @@ import Section1 from "./Section1";
 import Section2 from "./Section2";
 import Section3 from "./Section3";
 
+import "./Landing.css";
+
 const Landing = () => {
   return (
     <section id="landing" className="landing">
       <Section1 />
-
       <Section2 />
-
       <Section3 />
     </section>
   );
