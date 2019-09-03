e6ebc0dd1d616f9e837c015b4ade635e1a5736f1
hollisma <hollism@princeton.edu>
2019-09-03 14:14:55 -0400

    styled profile forms for columns and textareas

--- client/src/components/dashboard/settings/profile/EditProfile.js
+++ client/src/components/dashboard/settings/profile/EditProfile.js
@@ -108,129 +108,165 @@ const EditProfile = ({
         Tell us about yourself!
       </p>
       <small>* = required field</small>
-      <form className="ui form" onSubmit={e => onSubmit(e)}>
-        <p>* Name</p>
-        <div className="field">
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            value={name}
-            onChange={e => onChange(e)}
-            required
+      <form className="ui form equal width grid" onSubmit={e => onSubmit(e)}>
+        <div className="row">
+          <div className="column">
+            <p>* Name</p>
+            <div className="field">
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={name}
+                onChange={e => onChange(e)}
+                required
+              />
+            </div>
+          </div>
+          <div className="column">
+            <p>Email (cannot change)</p>
+            <div className="field">
+              <input
+                type="text"
+                placeholder="email"
+                name="email"
+                value={email}
+                disabled
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <p>* College</p>
+            <div className="field">
+              <input
+                type="text"
+                placeholder="College"
+                name="college"
+                value={college}
+                onChange={e => onChange(e)}
+                required
+              />
+            </div>
+          </div>
+          <div className="column">
+            <p>* Major</p>
+            <div className="field">
+              <input
+                type="text"
+                placeholder="Major"
+                name="major"
+                value={major}
+                onChange={e => onChange(e)}
+                required
+              />
+            </div>
+          </div>
+          <div className="column">
+            <p>Minor (separate by commas)</p>
+            <div className="field">
+              <input
+                type="text"
+                placeholder="Minor"
+                name="minor"
+                value={minor}
+                onChange={e => onChange(e)}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <p>Categories you're interested in</p>
+            <div className="field">
+              <input
+                type="text"
+                placeholder="Categories"
+                name="categories"
+                value={categories}
+                onChange={e => onChange(e)}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <p>
+              Tell us a little about yourself: hobbies, interests, work,
+              anything!
+            </p>
+            <div className="field">
+              <textarea
+                className="big-input"
+                placeholder="Bio"
+                name="bio"
+                value={bio}
+                onChange={e => onChange(e)}
+                rows="4"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <p>
+              In a couple sentences, describe the type of people you want to
+              meet
+            </p>
+            <div className="field">
+              <textarea
+                className="big-input"
+                placeholder="I want to meet people who are yada yada yada"
+                name="want_to_meet"
+                value={want_to_meet}
+                onChange={e => onChange(e)}
+                rows="4"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="column">
+          <p>Phone Number</p>
+          <PhoneInput
+            placeholder="Enter phone number"
+            country="US"
+            value={phone_number}
+            onChange={e => onPhoneChange(e)}
           />
         </div>
-        <p>Email (cannot change)</p>
-        <div className="field">
-          <input
-            type="text"
-            placeholder="email"
-            name="email"
-            value={email}
-            disabled
-            required
-          />
-        </div>
-        <p>* College</p>
-        <div className="field">
-          <input
-            type="text"
-            placeholder="College"
-            name="college"
-            value={college}
-            onChange={e => onChange(e)}
-            required
-          />
-        </div>
-        <p>* Major</p>
-        <div className="field">
-          <input
-            type="text"
-            placeholder="Major"
-            name="major"
-            value={major}
-            onChange={e => onChange(e)}
-            required
-          />
-        </div>
-        <p>Minor</p>
-        <div className="field">
-          <input
-            type="text"
-            placeholder="Minor"
-            name="minor"
-            value={minor}
-            onChange={e => onChange(e)}
-          />
-        </div>
-        <p>Categories you're interested in</p>
-        <div className="field">
-          <input
-            type="text"
-            placeholder="Categories"
-            name="categories"
-            value={categories}
-            onChange={e => onChange(e)}
-          />
-        </div>
-        <p>
-          Tell us a little about yourself: hobbies, interests, work, anything!
-        </p>
-        <div className="field">
-          <input
-            type="text"
-            placeholder="Bio"
-            name="bio"
-            value={bio}
-            onChange={e => onChange(e)}
-          />
-        </div>
-        <p>
-          In a couple sentences, describe the type of people you want to meet
-        </p>
-        <div className="field">
-          <input
-            type="text"
-            placeholder="I want to meet people who are yada yada yada"
-            name="want_to_meet"
-            value={want_to_meet}
-            onChange={e => onChange(e)}
-          />
-        </div>
-        <p>Phone Number</p>
-        <PhoneInput
-          placeholder="Enter phone number"
-          country="US"
-          value={phone_number}
-          onChange={e => onPhoneChange(e)}
-        />
-        <p>Communication Preference</p>
-        <div className="ui field toggle checkbox">
-          <input
-            type="checkbox"
-            name="comm_sms"
-            checked={comm_sms}
-            onChange={e => onChange(e)}
-          />
-          <label>Text</label>
-        </div>
-        <div className="ui field toggle checkbox mx-2 my">
-          <input
-            type="checkbox"
-            name="comm_email"
-            checked={comm_email}
-            onChange={e => onChange(e)}
-          />
-          <label>Email</label>
+        <div className="column">
+          <p>Communication Preference</p>
+          <div className="ui field toggle checkbox">
+            <input
+              type="checkbox"
+              name="comm_sms"
+              checked={comm_sms}
+              onChange={e => onChange(e)}
+            />
+            <label>Text</label>
+          </div>
+          <div className="ui field toggle checkbox mx-2 my">
+            <input
+              type="checkbox"
+              name="comm_email"
+              checked={comm_email}
+              onChange={e => onChange(e)}
+            />
+            <label>Email</label>
+          </div>
         </div>
 
-        <a href="/" className="ui red basic button m-1 right floated">
-          Go back
-        </a>
-        <input
-          type="submit"
-          className="ui green basic button my-1 right floated"
-        />
+        <div className="column">
+          <a href="/" className="ui red basic button m-1 right floated">
+            Go back
+          </a>
+          <input
+            type="submit"
+            className="ui green basic button my-1 right floated"
+          />
+        </div>
       </form>
     </div>
   );
