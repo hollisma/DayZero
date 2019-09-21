6e729de1f9765d0df150186ed25dd8d5d5d9b2bf
hollisma <hollism@princeton.edu>
2019-09-21 11:12:25 -0400

    created update user admin route

--- routes/api/users.js
+++ routes/api/users.js
@@ -168,4 +168,65 @@ router.get("/admin", admin, async (req, res) => {
   }
 });
 
+/**
+ * @route   PUT api/users/admin
+ * @desc    Update user
+ * @access  Public
+ * */
+router.put("/admin", admin, async (req, res) => {
+  const {
+    id,
+    name,
+    email,
+    avatar,
+    phone_number,
+    comm_sms,
+    comm_email,
+    vibe,
+    user_type,
+    group
+  } = req.body;
+  let { password } = req.body;
+
+  try {
+    // Check if user already exists
+    var user = await User.findById(id);
+    if (!user) {
+      return res.status(400).json({ errors: [{ msg: "User not found" }] });
+    }
+
+    if (password) {
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      password = await bcrypt.hash(password, salt);
+    }
+
+    let newUser = {};
+    if (id) newUser.id = id;
+    if (name) newUser.name = name;
+    if (email) newUser.email = email;
+    if (avatar) newUser.avatar = avatar;
+    if (phone_number) newUser.phone_number = phone_number;
+    if (comm_sms) newUser.comm_sms = comm_sms;
+    if (comm_email) newUser.comm_email = comm_email;
+    if (vibe) newUser.vibe = vibe;
+    if (user_type) newUser.user_type = user_type;
+    if (group) newUser.group = group;
+    if (password) newUser.password = password;
+
+    user = await User.findByIdAndUpdate(
+      id,
+      {
+        $set: newUser
+      },
+      { new: true }
+    );
+
+    return res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
+
 module.exports = router;
