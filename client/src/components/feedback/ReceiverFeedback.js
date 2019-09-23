1e5b6fed49c130cdd781442cee0c801b45d4dc58
ninjawearingnike <zkyu@princeton.edu>
2019-09-23 09:39:11 -0700

    feedback css

--- client/src/components/feedback/ReceiverFeedback.js
+++ client/src/components/feedback/ReceiverFeedback.js
@@ -1,7 +1,8 @@
 import React from "react";
-import StarRatingComponent from "react-star-rating-component";
 import PropTypes from "prop-types";
 import Avatar from "react-avatar";
+
+import "./Feedback.css";
 
 const ReceiverFeedback = ({
   name,
@@ -20,10 +21,11 @@ const ReceiverFeedback = ({
   return (
     <div className="receiver-feedback">
       <div className="info">
-        <Avatar className="avatar" size="100" round src={avatar} />
+        <Avatar className="avatar" size="40%" round src={avatar} />
         <div className="member">{name}</div>
       </div>
       <div className="binary">
+        <p id="question-one">Would you want to meet {name} again?</p>
         <button
           className={
             "ui green button " + ((binary == null || !binary) && "basic")
@@ -38,6 +40,15 @@ const ReceiverFeedback = ({
         >
           No
         </button>
+      </div>
+      <div className="superlike">
+        <p>I absolutely LOVED meeting this person!</p>
+        <div class="ui labeled button" tabindex="0">
+          <div class="ui red button">
+            <i class="heart icon"></i> Like
+          </div>
+          <a class="ui basic red left pointing label">1,048</a>
+        </div>
       </div>
     </div>
   );
