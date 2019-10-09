888375d671a436a20a1e728461aca3d618ffb776
ninjawearingnike <zkyu@princeton.edu>
2019-10-08 22:47:27 -0700

    section 2 css revamp

--- client/src/components/landing/Section2.js
+++ client/src/components/landing/Section2.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Section2 = () => {
   return (
-    <div id="howitworks" className="background pic chunk">
-      <div className="ui two column stackable center aligned grid">
-        <h1 className="title">How Day Zero Works</h1>
-        <div className="ui middle aligned stackable grid container howitworks-table">
+    <div id="howitworks">
+      {/* <div className="ui two column stackable center aligned grid">
+        <h1 className="title">How Day Zero Works</h1> */}
+      {/* <div className="ui middle aligned stackable grid container howitworks-table">
           <img
             src={require("../../img/rocket.svg")}
             className="svg-rocket"
@@ -31,9 +31,44 @@ const Section2 = () => {
             shares the most interests with you so y'all can communicate and
             figure out where to meet!
           </p>
-        </div>
+        </div> */}
+      <div className="steps">
+        <img
+          src={require("../../img/interests.PNG")}
+          className="select_interests"
+          alt="interests graphic"
+        />
+        <p>
+          <b>Step 1:</b> Select your interests from a list that spans
+          industries, academic disciplines, hobbies, and more.
+        </p>
+      </div>
+      <div className="steps">
+        <img
+          src={require("../../img/calendar.PNG")}
+          className="select_time"
+          alt="calendar graphic"
+        />
+        <p>
+          <b>Step 2:</b> Tell us what days you're free to grab lunch, coffee, or
+          dinner.
+        </p>
+      </div>
+      <div className="steps">
+        <img
+          src={require("../../img/people.svg")}
+          className="svg-people"
+          alt=""
+        />
+        <p>
+          <b>That's it!</b> Day Zero then automatically sends a group text to
+          you and the person who shares the most interests with you so y'all can
+          figure out where to meet! Your Day Zero dashboard will display their
+          bio so you can learn about them before the meeting.
+        </p>
       </div>
     </div>
+    // </div>
   );
 };
 
--- client/src/components/landing/Section3.js
+++ client/src/components/landing/Section3.js
@@ -12,22 +12,20 @@ const Section3 = () => {
             interests XYZ and request meetings with them. Stay tuned!
           </p>
           <h2 className="question">
+            What information will be displayed to other users?
+          </h2>
+          <p className="answer">
+            Only your interests and bio are displayed to others for the purpose
+            of helping your matches know more about you before the meeting. No
+            private information will ever be displayed to others.
+          </p>
+          <h2 className="question">
             Can we sign up if we don't go to Stanford or Princeton?
           </h2>
           <p className="answer">
             If you would like Day Zero to launch at your university, shoot us an
             email <a href="mailto:founders.dayzero@gmail.com">here</a> and we
             will expand access as soon as possible.{" "}
-          </p>
-          <h2 className="question">
-            What information will be displayed to other users?
-          </h2>
-          <p className="answer">
-            After meeting someone, you have the option of sending them some
-            emojis that describe how cool you thought they were. Other than
-            that, only your interests and bio are displayed to others for the
-            purpose of helping your matches know more about you before the
-            meeting. No private information will ever be displayed to others.
           </p>
         </div>
       </div>
