deeadfe6f35c6ab381d74c0c8906af114c8773aa
ninjawearingnike <zkyu@princeton.edu>
2019-10-12 20:53:19 -0700

    matching notifier update

--- alg/matching_notifier.py
+++ alg/matching_notifier.py
@@ -1,22 +1,32 @@
 from dayzero_gmail import DayZeroGmail
 import textwrap
 
+
 class MatchingNotifier:
     def __init__(self):
         self.email = DayZeroGmail()
 
     def match(self, profiles):
         recipient_emails = [p['user']['email'] for p in profiles]
-        subject = "You've matched!"
-        self.email.send(recipient_emails, subject, self.matching_message(profiles))
+        subject = "Your Day Zero match says hi!"
+        self.email.send(recipient_emails, subject,
+                        self.matching_message(profiles))
 
     def matching_message(self, profiles):
-        #Please feel free to change the content of message using the information in profiles
+        # Please feel free to change the content of message using the information in profiles
         recipient_names = [p['user']['name'] for p in profiles]
-        recipient_names_str = "{} and {}".format(', '.join(recipient_names[:-1]), recipient_names[-1])
+        recipient_names_str = "{} and {}".format(
+            ', '.join(recipient_names[:-1]), recipient_names[-1])
         body = """
-        Hello, {}!
-        You've matched on DayZero based on your interest! Please talk about what you are interested in via email!
+        Hey {user1.name} and {user2.name}!
+        I'm really excited to be introducing you two. You guys seemed like such a great fit 
+        it would've been a crime for you two not to meet! Now that you guys have each other's
+        emails, you can trade numbers or use this email thread to schedule a specific
+        time and place. 
+        
+        You guys both said you were interested in: [insert topics]
+        You guys both said you were free for meals on [type of meal on this date, type of meal on this date, etc.]
+
+        Go to dayzero.college to read your day zero's bio.
         """.format(recipient_names_str)
         return textwrap.dedent(body).strip()
-
