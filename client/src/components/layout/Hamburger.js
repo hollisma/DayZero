54d2487ebc2ccea6cf7f04a8a5f899c11e3c4cb9
hollisma <hollism@princeton.edu>
2019-10-13 15:02:15 -0400

    added state to hamburger. logout not working

--- client/src/components/layout/Hamburger.js
+++ client/src/components/layout/Hamburger.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import { logout } from "../../actions/auth";
 
 import "./Hamburger.css";
 
 const Hamburger = ({ authenticated }) => {
+  const [toggled, setToggled] = useState(false);
+
   if (authenticated) {
-    console.log("authenticated :)");
     return (
       <div className="hamburger">
         <nav role="navigation">
-          <div id="menuToggle">
+          <div id="menuToggle" className={toggled ? "toggled" : ""}>
             {/* A fake / hidden checkbox is used as click reciever,
             so you can use the :checked selector on it. */}
-            <input type="checkbox" />
+            <input type="checkbox" onClick={() => setToggled(!toggled)} />
 
             {/* Some spans to act as a hamburger.
             
@@ -26,18 +26,21 @@ const Hamburger = ({ authenticated }) => {
             {/* Too bad the menu has to be inside of the button
             but hey, it's pure CSS magic. */}
             <ul id="menu">
-              <a href="/dashboard#">
+              <a href="/dashboard#" onClick={() => setToggled(false)}>
                 <li>Your Day Zeros</li>
               </a>
-              <a href="/dashboard#settings">
+              <a href="/dashboard#settings" onClick={() => setToggled(false)}>
                 <li>Settings</li>
               </a>
-              <a href="/#howitworks">
+              <a href="/#howitworks" onClick={() => setToggled(false)}>
                 <li>How It Works</li>
               </a>
-              <Link onClick={logout} to="/#">
+              <a href="/#" onClick={logout}>
+                <li>Logout</li>
+              </a>
+              {/* <Link onClick={logout} to="/#">
                 <i className="fas fa-sign-out-alt" /> <li>Logout</li>
-              </Link>
+              </Link> */}
             </ul>
           </div>
         </nav>
@@ -47,10 +50,10 @@ const Hamburger = ({ authenticated }) => {
     return (
       <div className="hamburger">
         <nav role="navigation">
-          <div id="menuToggle">
+          <div id="menuToggle" className={toggled ? "toggled" : ""}>
             {/* A fake / hidden checkbox is used as click reciever,
             so you can use the :checked selector on it. */}
-            <input type="checkbox" />
+            <input type="checkbox" onClick={() => setToggled(!toggled)} />
 
             {/* Some spans to act as a hamburger.
             
@@ -63,18 +66,19 @@ const Hamburger = ({ authenticated }) => {
             {/* Too bad the menu has to be inside of the button
             but hey, it's pure CSS magic. */}
             <ul id="menu">
-              <a href="/#">
+              <a href="/#" onClick={() => setToggled(false)}>
                 <li>Home</li>
               </a>
-              <a href="/#howitworks">
+              <a href="/#howitworks" onClick={() => setToggled(false)}>
                 <li>How It Works</li>
               </a>
-              <a href="/#faq">
+              <a href="/#faq" onClick={() => setToggled(false)}>
                 <li>FAQ/About Us</li>
               </a>
               <a
                 href="https://medium.com/@kelvinotcelsius/a-letter-from-the-founders-introducing-day-zero-428b591fc778"
                 // target="_blank"
+                onClick={() => setToggled(false)}
               >
                 <li>Our Letter</li>
               </a>
