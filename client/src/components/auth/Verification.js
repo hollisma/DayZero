0a3bee4b15d84ce5b6eae9b38ff639ecc175cc4c
hollisma <hollism@princeton.edu>
2019-11-04 20:56:47 -0500

    trying to get gmail to work for verification

--- client/src/components/auth/Verification.js
+++ client/src/components/auth/Verification.js
@@ -1,6 +1,5 @@
 import React from "react";
 import queryString from "query-string";
-import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { verification } from "../../actions/auth";
--- utils/DayZeroGmail.js
+++ utils/DayZeroGmail.js
@@ -1,30 +1,43 @@
-require('dotenv').config()
-let nodemailer = require('nodemailer')
+require("dotenv").config();
+let nodemailer = require("nodemailer");
 
 module.exports = class DayZeroGmail {
-    constructor() {
-        this.mail_address = process.env.EMAIL_ADDRESS
-    }
-    async send(recipients, subject, body) {
-        let msg = {
-            from: this.mail_address,
-            to: recipients,
-            subject: subject,
-            text: body,
-        }
+  constructor() {
+    this.mail_address = process.env.EMAIL_ADDRESS;
+  }
+  async send(recipients, subject, body) {
+    let msg = {
+      from: this.mail_address,
+      to: recipients,
+      subject: subject,
+      text: body
+    };
 
-        let config = {
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: this.mail_address,
-                pass: process.env.PASSWORD,
-            },
-        }
-        let transporter = nodemailer.createTransport(config)
-        transporter.sendMail(msg, function(err, response) {
-            console.log(err || response)
-        })
-    }
-}
+    // let config = {
+    //   host: "smtp.gmail.com",
+    //   port: 465,
+    //   secure: true,
+    //   auth: {
+    //     type: "OAuth2",
+    //     user: this.mail_address,
+    //     clientId:
+    //       "899676570691-3161ugk2b3nvnip3hahgfk4dtpkl9qg8.apps.googleusercontent.com",
+    //     clientSecret: "zh2drPx7yZWpwMgabYUu0lvy"
+    //   }
+    // };
+    let config = {
+      host: "smtp.gmail.com",
+      port: 465,
+      // secure: true,
+      secure: false,
+      auth: {
+        user: this.mail_address,
+        pass: process.env.PASSWORD
+      }
+    };
+    let transporter = nodemailer.createTransport(config);
+    transporter.sendMail(msg, function(err, response) {
+      console.log(err || response);
+    });
+  }
+};
