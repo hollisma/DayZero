b7fe3e1c5eff793973d5feb610b3c58129b2ebf5
ninjawearingnike <zkyu@princeton.edu>
2019-11-30 13:06:06 -0800

    css

--- client/src/components/landing/LandingDesktop.js
+++ client/src/components/landing/LandingDesktop.js
@@ -26,7 +26,7 @@ const LandingDesktop = ({ isAuthenticated }) => {
       <div className="middle">
         <div className="how-we-help">
           <div className="title">
-            We help you quickly discover the people most relevant to you.
+            Quickly discover the people most relevant to you.
           </div>
           <div className="text">
             There are <span className="colorful">8,364</span> students at
@@ -37,27 +37,28 @@ const LandingDesktop = ({ isAuthenticated }) => {
             <span className="colorful">4.3%</span> of them.
           </div>
           <div className="text">
-            <span className="colorful">Day Zero</span> connects you with people
-            you'd most vibe with but have never met.
+            <span className="colorful">Day Zero</span> makes it easy to discover
+            and grab meals with the people that share your passions and values
+            but you've never met.
           </div>
         </div>
         <div className="people-cards">
           <div className="card-profile">
             <img
-              src={require("../../img/carina_rec.PNG")}
+              src={require("../../img/carina_rec.svg")}
               alt="Carina"
               className="card-pic"
             />
             <div className="text">
-              "I recently got into fintech but I didn't know ppl passionate
-              about it. But through DZ I got dinner with a really cool guy who
-              has a fintech startup!"
+              Day Zero made it easy for me to find other people passionate about
+              fintech. Last week I got dinner with a guy who has his own fintech
+              startup!"
             </div>
             <div className="name">Carina L. '21</div>
           </div>
           <div className="card-profile alt-background">
             <img
-              src={require("../../img/dan_rec.PNG")}
+              src={require("../../img/dan_rec.svg")}
               alt="Dan"
               className="card-pic"
             />
@@ -66,20 +67,20 @@ const LandingDesktop = ({ isAuthenticated }) => {
               mindfulness, but boy was I wrong. Day Zero helped me find such
               cool people hidden in plain sight."
             </div>
-            <div className="name alt-text">Dan '21</div>
+            <div className="name alt-text">Dan K. '21</div>
           </div>
           <div className="card-profile">
             <img
-              src={require("../../img/marti_rec.PNG")}
+              src={require("../../img/marti_rec.svg")}
               alt="Marti"
               className="card-pic"
             />
             <div className="text">
               "After joining TI it was hard to meet new people outside the club,
-              but through Day Zero, I've made great friends I prob would've
-              never met before."
+              but through DZ, I've met fantastic people I prob would've never
+              ran into otherwise."
             </div>
-            <div className="name">Marti '20</div>
+            <div className="name">Marti V. '20</div>
           </div>
         </div>
       </div>
@@ -89,14 +90,16 @@ const LandingDesktop = ({ isAuthenticated }) => {
           <div className="cards">
             <div className="card-background">
               <div className="card">
-                Search for peers based on shared interests
+                Discover peers who share your passions and values
+              </div>
+            </div>
+            <div className="card-background">
+              <div className="card">
+                Get automatically matched with like-minded people for a meal
               </div>
             </div>
             <div className="card-background">
               <div className="card">Find out who are in your classes</div>
-            </div>
-            <div className="card-background">
-              <div className="card">Schedule meals with like-minded people</div>
             </div>
           </div>
           <img
@@ -109,17 +112,16 @@ const LandingDesktop = ({ isAuthenticated }) => {
           <a href="/sign-up-landing" className="sign-up-button">
             Sign up
           </a>
-          <div className="questions">Questions? Ask Away~</div>
-          <div className="contact-us">
-            Email us{" "}
+          <div className="questions">
+            If you have feedback or questions, we'd love to hear from you! Email
+            us{" "}
             <a
               href="mailto:founders.dayzero@gmail.com"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <u>here</u>
+              <u>here.</u>
             </a>
-            !
           </div>
         </div>
       </div>
--- client/src/components/landing/LandingMobile.js
+++ client/src/components/landing/LandingMobile.js
@@ -22,28 +22,32 @@ const LandingMobile = ({ isAuthenticated }) => {
         </a>
       </div>
       <div className="middle">
+        <div className="title">
+          Quickly discover the people most relevant to you.
+        </div>
         <div className="text">
           There are <span className="colorful">8,364</span> students at
           Princeton.
         </div>
         <div className="text">
-          In 4 years, you only meet <span className="colorful">4%</span> of
+          In 4 years, you only meet <span className="colorful">4.3%</span> of
           them.
         </div>
         <div className="text">
-          <span className="colorful">Day Zero</span> connects you with people
-          you'd most vibe with but have never met.
+          <span className="colorful">Day Zero</span> makes it easy to discover
+          and grab meals with the people that share your passions and values but
+          you've never met.
         </div>
         <div className="card-profile">
           <img
-            src={require("../../img/carina_rec.PNG")}
+            src={require("../../img/carina_rec.svg")}
             alt="Carina"
             className="card-pic"
           />
           <div className="text">
-            "I recently got into fintech but I didn't know ppl passionate about
-            it. But through DZ I got dinner with a really cool guy who has a
-            fintech startup!"
+            Day Zero made it easy for me to find other people passionate about
+            fintech. Last week I got dinner with a guy who has his own fintech
+            startup!"
           </div>
           <div className="name">Carina '21</div>
         </div>
@@ -52,34 +56,32 @@ const LandingMobile = ({ isAuthenticated }) => {
         <div className="transition">With Day Zero, you can...</div>
         <div className="card-background">
           <div className="card-possibility">
-            Search for peers based on shared interests
+            Find peers who share your passions and values
           </div>
         </div>
         <div className="card-background">
           <div className="card-possibility">
-            Find out who are in your classes
+            Get automatically matched with like-minded people for a meal
           </div>
         </div>
         <div className="card-background">
           <div className="card-possibility">
-            Schedule meals with like-minded people
+            Find out who are in your classes{" "}
           </div>
         </div>
         <a href="/sign-up-landing" className="sign-up-button">
           Sign up
         </a>
-        <div className="questions">Questions?</div>
-        <div className="questions">Ask away~</div>
-        <div className="contact-us">
-          Email us Email us{" "}
+        <div className="questions">
+          If you have feedback or questions, we'd love to hear from you! Email
+          us{" "}
           <a
             href="mailto:founders.dayzero@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <u>here</u>
+            <u>here.</u>
           </a>
-          !
         </div>
       </div>
     </div>
