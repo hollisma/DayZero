9978b0dfc6d8aa0e630bffa16c6ca6d0cdcc2656
ninjawearingnike <zkyu@princeton.edu>
2019-12-01 08:16:50 -0800

    algo script

--- alg/matching_notifier.py
+++ alg/matching_notifier.py
@@ -9,14 +9,17 @@ class MatchingNotifier:
     def match(self, profiles, times, categories):
         recipient_emails = [p['user']['email'] for p in profiles]
         subject = "You've matched!"
-        self.email.send(recipient_emails, subject, self.matching_message(profiles, times, categories), "logo.png")
+        self.email.send(recipient_emails, subject, self.matching_message(
+            profiles, times, categories), "logo.png")
 
     def matching_message(self, profiles, times, categories):
         # Please feel free to change the content of message using the information in profiles
         recipient_names = [p['user']['name'] for p in profiles]
-        recipient_names_str = "{} and {}".format(', '.join(recipient_names[:-1]), recipient_names[-1])
+        recipient_names_str = "{} and {}".format(
+            ', '.join(recipient_names[:-1]), recipient_names[-1])
         times_str = "{} and {}".format(', '.join(times[:-1]), times[-1])
-        categories_str = "{} and {}".format(', '.join(categories[:-1]), categories[-1])
+        categories_str = "{} and {}".format(
+            ', '.join(categories[:-1]), categories[-1])
         body = """
         Hey {}!
 
@@ -25,10 +28,13 @@ class MatchingNotifier:
         emails, you can trade numbers or use this email thread to schedule a specific
         time and place. 
         
-        You both are interested in: {}
-        You both are free for meals on {}
+        You both identified as: {}
+        You are both free for meals on {}
 
         Hope you have a blast meeting each other!
+
+        Sincerely, 
+        
         Day Zero
 
         """.format(recipient_names_str, categories_str, times_str)
