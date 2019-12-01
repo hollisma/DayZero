371933d1834b2d7f7d54d413ae3c30b2d56b5845
ninjawearingnike <zkyu@princeton.edu>
2019-11-30 23:12:22 -0800

    how it works

--- client/src/components/landing/Section2.js
+++ client/src/components/landing/Section2.js
@@ -3,42 +3,50 @@ import React from "react";
 const Section2 = () => {
   return (
     <div id="howitworks">
-      <div className="steps">
-        <img
-          src={require("../../img/interests.PNG")}
-          className="select_interests"
-          alt="interests graphic"
-        />
-        <p>
-          <b>Step 1:</b> Select your interests from a list that spans
-          industries, academic disciplines, hobbies, and more.
+      <div className="faq">
+        <p className="faq-title">
+          <b>How automatic meal matches work</b>
+        </p>
+        <p className="faq-body">
+          In addition to manually searching for users on the Day Zero network
+          with our 'search' feature, we can also match you for a meal
+          automatically with a someone who shares your passions.
+          <br></br>
+          <br></br>
+          To get an automatic match, go to your Day Zero dashboard and fill out
+          when you're free for lunch or dinner. Based on who you've liked (see
+          below) and what categories you identified with, DZ will automatically
+          find an optimal match. Once found, we will send you both an
+          introductory email, and then you guys can figure out the specific
+          meeting place and time.
         </p>
       </div>
-      <div className="steps">
-        <img
-          src={require("../../img/calendar.png")}
-          className="select_time"
-          alt="calendar graphic"
-        />
-        <p>
-          <b>Step 2:</b> Tell us what days you're free to grab lunch or dinner.
+      <div className="faq">
+        <p className="faq-title">
+          <b>What are likes?</b>
+        </p>
+        <p className="faq-body">
+          If you go to someone's profile, you will see a red 'Like' button. Its
+          purpose is for you to tell us what kind of person you enjoy meeting so
+          we can recommend you people you'd vibe with but don't necessarily
+          share a lot of commonalities with. For example, if you have a close
+          friend that has different interests than you but you would still like
+          to engage with people like them, you'd 'like' them and DZ will
+          incorporate that in its recommendation engine.
         </p>
       </div>
-      <div className="steps">
-        <img
-          src={require("../../img/people.svg")}
-          className="svg-people"
-          alt=""
-        />
-        <p>
-          <b>Step 3: That's it!</b> Day Zero then automatically sends an email
-          to you and the person who <b>shares the most interests</b> with you so
-          y'all can figure out where to meet! Your Day Zero dashboard will
-          display their bio so you can learn about them before the meeting.
+      <div className="faq">
+        <p className="faq-title">
+          <b>How does search work?</b>
+        </p>
+        <p className="faq-body">
+          Search's purpose is to help you efficiently find people that have any
+          combination of characteristics. You can filter based on your heart's
+          desire, and once you go to someone's profile, you can click 'Say Hi'
+          to reach out to them.
         </p>
       </div>
     </div>
-    // </div>
   );
 };
 
