f485f0976ce98b9f7ab6fa373e92aec8ac3f0525
hollisma <hollism@princeton.edu>
2021-01-30 13:39:08 -0500

    clean_schedules updated

--- alg/clean_schedules.py
+++ alg/clean_schedules.py
@@ -10,21 +10,19 @@ host_name = 'http://172.31.43.129'
 # host_name = 'http://localhost'
 
 now = datetime.now()
+load_dotenv()
 
 # Authentication
-load_dotenv()
 url = host_name + ':5000/api/auth'
 body = { 'email': os.getenv('ADMIN_EMAIL'), 'password': os.getenv('ADMIN_PASSWORD') }
-response = requests.post(url, json=body).json()
-token = response['token']
-
-headers = { 'x-auth-token': token }
+response = requests.post(url, json=body)
+res = json.loads(response.text)
+headers = { 'x-auth-token': res['token']}
 
 # Get all users' schedules
-url = host_name + ':5000/api/schedule/admin'
+url = host_name + ':5000/api/matchInfo/admin'
 response = requests.get(url, headers=headers)
-print(response.text)
-schedules = json.loads(response.text)
+matchInfos = json.loads(response.text)
 
 def toDatetime(t):
     nums = t.split(',')[0].split('-')
@@ -32,15 +30,14 @@ def toDatetime(t):
     return dt
 
 def updateSchedule(times, user):
-    url = host_name + ':5000/api/schedule/admin'
+    url = host_name + ':5000/api/matchInfo/admin'
     body = { 'times': times, 'user': user }
-    response = requests.post(url, headers=headers)
+    response = requests.post(url, headers=headers, json=body)
     response = json.loads(response.text)
-    print(response)
 
-for schedule in schedules:
-    times = schedule['times']
-    user = schedule['user']
+for matchInfo in matchInfos:
+    times = matchInfo['times']
+    user = matchInfo['user']
 
     times = list(filter(lambda t: toDatetime(t) > datetime.now(), times))
     updateSchedule(times, user)
